6bc4f98294ec30b688e2007d3c49136fb53247ba
haji4ref <haji4ref@gmail.com>
2025-04-20 11:43:23 +0330

    chore: fix callback passing into render function

--- src/lib/components/ArcaptchaWidget.js
+++ src/lib/components/ArcaptchaWidget.js
@@ -16,24 +16,6 @@ class ArCaptcha extends Component {
     id: "",
   };
 
-  componentWillUnmount() {
-    let callbacks = [];
-    for (var prop in window)
-      if (
-        prop.includes("arcaptcha_callback_") ||
-        prop.includes("arcaptcha_rendered_callback_") ||
-        prop.includes("arcaptcha_closed_callback_") ||
-        prop.includes("arcaptcha_opened_callback_") ||
-        prop.includes("arcaptcha_error_callback_") ||
-        prop.includes("arcaptcha_reset_callback_") ||
-        prop.includes("arcaptcha_expired_callback_") ||
-        prop.includes("arcaptcha_chlexpired_callback_")
-      )
-        callbacks.push(prop);
-    callbacks.forEach((arr) => {
-      delete window[arr];
-    });
-  }
   componentDidMount() {
     //Once captcha is mounted intialize ArCaptcha
     this.setID();
@@ -53,7 +35,7 @@ class ArCaptcha extends Component {
         };
       });
     }
-    if (my_script) {
+    if (my_script || window.arcaptcha) {
       window.arcaptchaWidgetLoading.then(() => {
         this.initialize();
       });
@@ -81,67 +63,18 @@ class ArCaptcha extends Component {
   execute() {
     return window.arcaptcha.execute(this.state.widget_id);
   }
-  registerCallback() {
-    if (this.props.callback)
-      window[`arcaptcha_callback_${this.state.id}`] = this.props.callback;
-
-    if (this.props.rendered_callback)
-      window[`arcaptcha_rendered_callback_${this.state.id}`] =
-        this.props.rendered_callback;
-
-    if (this.props.closed_callback)
-      window[`arcaptcha_closed_callback_${this.state.id}`] =
-        this.props.closed_callback;
-
-    if (this.props.opened_callback)
-      window[`arcaptcha_opened_callback_${this.state.id}`] =
-        this.props.opened_callback;
-
-    if (this.props.error_callback)
-      window[`arcaptcha_error_callback_${this.state.id}`] =
-        this.props.error_callback;
-
-    if (this.props.reset_callback)
-      window[`arcaptcha_reset_callback_${this.state.id}`] =
-        this.props.reset_callback;
-
-    if (this.props.expired_callback)
-      window[`arcaptcha_expired_callback_${this.state.id}`] =
-        this.props.expired_callback;
-
-    if (this.props.chlexpired_callback)
-      window[`arcaptcha_chlexpired_callback_${this.state.id}`] =
-        this.props.chlexpired_callback;
-  }
   loadCaptcha() {
-    this.registerCallback();
     const widgetId = window.arcaptcha.render(`#${this.state.id}`, {
       "site-key": this.props["site-key"],
       size: this.props.invisible ? "invisible" : "",
-      callback: this.props.callback
-        ? `arcaptcha_callback_${this.state.id}`
-        : false,
-      rendered_callback: this.props.rendered_callback
-        ? `arcaptcha_rendered_callback_${this.state.id}`
-        : false,
-      closed_callback: this.props.closed_callback
-        ? `arcaptcha_closed_callback_${this.state.id}`
-        : false,
-      opened_callback: this.props.opened_callback
-        ? `arcaptcha_opened_callback_${this.state.id}`
-        : false,
-      error_callback: this.props.error_callback
-        ? `arcaptcha_error_callback_${this.state.id}`
-        : false,
-      reset_callback: this.props.reset_callback
-        ? `arcaptcha_reset_callback_${this.state.id}`
-        : false,
-      expired_callback: this.props.expired_callback
-        ? `arcaptcha_expired_callback_${this.state.id}`
-        : false,
-      chlexpired_callback: this.props.chlexpired_callback
-        ? `arcaptcha_chlexpired_callback_${this.state.id}`
-        : false,
+      callback: this.props.callback,
+      rendered_callback: this.props.rendered_callback,
+      closed_callback: this.props.closed_callback,
+      opened_callback: this.props.opened_callback,
+      error_callback: this.props.error_callback,
+      reset_callback: this.props.reset_callback,
+      expired_callback: this.props.expired_callback,
+      chlexpired_callback: this.props.chlexpired_callback,
       lang: this.props.lang,
       theme: this.props.theme,
       color: this.props.color,
